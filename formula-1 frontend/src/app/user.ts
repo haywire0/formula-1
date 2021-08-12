4c9a5f8deae740683c8aeece7edf449640cec7db
haywire0 <88825250+haywire0@users.noreply.github.com>
2021-08-12 14:55:45 +0530

    Add files via upload

---
+++ formula-1 frontend/src/app/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    position: number | undefined;
+    name: string | undefined;
+    points: number | undefined;
+    teamName: string | undefined;
+    engineUsed: string | undefined;
+    tires: string | undefined;
+}
